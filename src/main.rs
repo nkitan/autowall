83c6c1c2429ea08650e0e65d73faee3cc8c47d4b
Ankit Das <ankitdas2k@gmail.com>
2025-01-08 17:54:09 +0530

    Refactor | Added variable to choose count of wallpapers picked

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate dotenv;
 mod lib;
+use std::u8;
+
 use lib::unsplash::integration as ui;
 use lib::download::download_file;
 use lib::place::place_wallpapers;
@@ -9,9 +11,10 @@ async fn main() {
     dotenv::from_filename("autowall.env").ok();
     let wallpapers_dir: String = std::env::var("WALLPAPER_STORAGE_LOCATION").expect("WALLPAPER_STORAGE_LOCATION is not set!");
     let wallpapers_query: String = std::env::var("WALLPAPER_QUERY").expect("WALLPAPER_QUERY is not set!");
+    let wallpapers_count: u8 = 3;
 
     // Query wallpapers_query and download 2 random photos to wallpapers_dir
-    match ui::get_random_photo(2, wallpapers_query).await {
+    match ui::get_random_photo(wallpapers_count, wallpapers_query).await {
         Ok(photos) => {
             for photo in photos {
                 let filename: String = format!("{}/{}.jpg", wallpapers_dir, photo.id);
