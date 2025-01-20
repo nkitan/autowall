914e7e6ee64566fb0c443fe3d4c94f7106064f26
Ankit Das <ankitdas2k@gmail.com>
2025-01-20 23:04:34 +0530

    Updated Local Wallpapers Directory

--- scripts/place.sh
+++ scripts/place.sh
@@ -3,6 +3,6 @@
 # place wallpapers in C:/Users/<USERNAME>/Pictures/Wallpapers/ directorires
 
 DEST=/mnt/c/Users/nkitan/Pictures/Wallpapers/
-SRC=/home/nkitan/Work/autowall/wallpapers/
+SRC=/opt/autowall/wallpapers/
 
 cp $SRC/* $DEST/
